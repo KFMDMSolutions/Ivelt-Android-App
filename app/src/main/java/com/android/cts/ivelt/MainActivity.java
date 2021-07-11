f82980d456772fb14cc710370b321ea6ef7ffc79
KF MDM Solutions <contact@kfmdmsolutions.com>
2021-07-11 07:52:09 -0400

    Revert "Updated to gradle 4.2.2, and migrated to Androidx"

--- app/src/main/java/com/android/cts/ivelt/MainActivity.java
+++ app/src/main/java/com/android/cts/ivelt/MainActivity.java
@@ -18,10 +18,10 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import androidx.annotation.NonNull;
-import androidx.core.content.ContextCompat;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
-import androidx.appcompat.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.ContextMenu;
@@ -53,9 +53,18 @@ public class MainActivity extends AppCompatActivity {
     SwipeRefreshLayout swipeRefreshLayout;
     String currentUrl = "https://www.ivelt.com/";
     String url = null;
+
+
+
+
+
     private static final int REQUEST_CODE_ALBUM = 1;
     private static final int REQUEST_CODE_CAMERA = 2;
+
+
     private ValueCallback<Uri[]> mFilePathCallback;
+
+
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
