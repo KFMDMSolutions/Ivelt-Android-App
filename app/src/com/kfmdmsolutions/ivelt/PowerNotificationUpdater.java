127ec308fa9297174a9fabd360a9350e54b4e248
KF MDM Solutions <contact@kfmdmsolutions.com>
2021-07-20 20:04:33 -0400

    Add files via upload

---
+++ app/src/com/kfmdmsolutions/ivelt/PowerNotificationUpdater.java
@@ -0,0 +1,25 @@
+package com.kfmdmsolutions.ivelt;
+
+import android.content.Context;
+import android.os.BatteryManager;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class PowerNotificationUpdater extends Worker {
+    public PowerNotificationUpdater(@NonNull Context context, @NonNull  WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+//        BatteryManager batteryManager = (BatteryManager) getApplicationContext().getSystemService(Context.BATTERY_SERVICE);
+//        if (batteryManager.isCharging()){
+        android.util.Log.d("NotifUpdate", "power work");
+            NotificationService.checkForNotifications(getApplicationContext());
+//        }
+        return Result.success();
+    }
+}
---
+++ app/src/com/kfmdmsolutions/ivelt/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.kfmdmsolutions.ivelt;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class SplashActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 7000;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
