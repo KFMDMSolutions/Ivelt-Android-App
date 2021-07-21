66b2af6e6c873bb3417afdaf7746fbc81f90e755
KF MDM Solutions <contact@kfmdmsolutions.com>
2021-07-20 20:07:38 -0400

    Add files via upload

---
+++ app/src/main/java/com/kfmdmsolutions/ivelt/SettingsFragment.java
@@ -0,0 +1,32 @@
+package com.kfmdmsolutions.ivelt;
+
+import android.os.Bundle;
+import android.text.InputFilter;
+import android.text.InputType;
+
+import androidx.preference.EditTextPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.settings, rootKey);
+        delayPreferenceChanged("plugged_in");
+        delayPreferenceChanged("pause_notifications");
+        delayPreferenceChanged("battery");
+    }
+
+    private void delayPreferenceChanged(String key) {
+        Preference preference = findPreference(key);
+        if (preference == null) return;
+        preference.setOnPreferenceChangeListener(
+                (changedPreference, newValue) -> {
+                   NotificationService.startNotificationService(getContext(), NotificationService.ACTION_UPDATE_DELAY_TIME);
+                    return true;
+                }
+                );
+    }
+
+}
